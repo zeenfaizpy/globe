998934580d2b164592b9a58e1645475ce72bc580
Mohammad Faizal <zeenfaiz@gmail.com>
2020-09-09 09:52:06 +0400

    update code

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 var data = [
-    //{lat:40.7765233, lng:-112.0605687, label: "Salt Lake City"},
-    //{lat:38.758814, lng:-124.1197801, label: "NiCasio"},
-    //{lat:25.0750853, lng:54.9475566, label: "Dubai"},
+    {lat:42.7542521, lng:-105.3966345, label: "Salt Lake City"},
+    {lat:38.758814, lng:-124.1197801, label: "NiCasio"},
+    {lat:25.0750853, lng:54.9475566, label: "Dubai"},
 ]
